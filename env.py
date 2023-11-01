8009d5ab7bb7b22113d99a779d7315092608eea7
ellrobot <147643352+ellrobot@users.noreply.github.com>
2023-11-01 17:05:06 +0900

    Update env.py

--- env.py
+++ env.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = os.getenv("API_ID", "").strip()
-API_HASH = os.getenv("API_HASH", "").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "")
+API_ID = os.getenv("API_ID", "17250424").strip()
+API_HASH = os.getenv("API_HASH", "753bc98074d420ef57ddf7eb1513162b").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6830518422:AAEPRM-cfMZjZk6yCHqg61aY3Q-qqONgot0").strip()
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://1234:1234@cluster0.gb6uhwg.mongodb.net/?retryWrites=true&w=majority").strip()
+MUST_JOIN = os.getenv("MUST_JOIN", "https://t.me/zyriccbase")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
